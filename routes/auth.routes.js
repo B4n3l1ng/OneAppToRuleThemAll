0b61c3967450cfb909f5bc541bab0417f645beb9
B4n3l1ng <114490314+B4n3l1ng@users.noreply.github.com>
2022-11-03 12:17:18 +0000

    sign up

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("../models/User.model");
 
+router.get("/signup", (req, res) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", async (req, res) => {
+  try {
+    const salt = bcrypt.genSaltSync(12);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+    if (await User.findOne({ username: req.body.username })) {
+      const errorMessage = "The username is already in use";
+      res.render("auth/signup", { errorMessage });
+    } else {
+      if (await User.findOne({ email: req.body.email })) {
+        const errorMessage = "The email is already in use";
+        res.render("auth/signup", { errorMessage });
+      } else {
+        await User.create({
+          username: req.body.username,
+          email: req.body.email,
+          password: hashedPassword,
+        });
+        res.redirect("/login");
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
