ef87fdd7db16e58c52952aaba9ad137c334e1278
B4n3l1ng <114490314+B4n3l1ng@users.noreply.github.com>
2022-11-03 13:49:52 +0000

    merge complete

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
 const User = require("../models/User.model");
 const bcrypt = require("bcryptjs");
-=======
-const bcrypt = require("bcryptjs");
-const User = require("../models/User.model")
->>>>>>> master
 
 router.get("/login", (req, res) => {
   res.render("auth/login");
@@ -31,7 +26,6 @@ router.post("/login", async (req, res) => {
   }
 });
 
-<<<<<<< HEAD
 router.get("/logout", (req, res, next) => {
   req.session.destroy((err) => {
     if (err) {
@@ -40,31 +34,6 @@ router.get("/logout", (req, res, next) => {
     res.redirect("/login");
   });
 });
-=======
-        else {
-            if (bcrypt.compareSync(password, currentUser.password)) {
-                req.session.user = currentUser
-                res.redirect("/profile")
-            } else {
-                res.render("auth/login", {errorMessage: "Incorrect password"})
-            }
-        }}
-        catch (error) {
-            console.log(error)
-        }
- })
-
-
- router.get("/logout", (req, res, next) => {
-    req.session.destroy(err => {
-        if (err) {
-            next(err)
-        }
-        res.redirect("/login")
-    })
- })
-
->>>>>>> master
 
 router.get("/signup", (req, res) => {
   res.render("auth/signup");
