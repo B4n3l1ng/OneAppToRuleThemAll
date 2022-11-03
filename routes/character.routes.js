7190a5089852468dd2c4722a7bd291d41809ffe3
Phoebe Orwell <phoebe.orwell@gmail.com>
2022-11-03 17:56:05 +0100

    Bugs fixed

--- routes/character.routes.js
+++ routes/character.routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const Character = require("../models/Character.model");
 
 router.get("/characters", async (req, res) => {
-  const currentUser = req.session.user;
-  const characters = await Character.find({ owner: currentUser });
+  const currentUserId = req.session.user.id;
+  const characters = await Character.find({ owner: currentUserId });
   console.log(characters);
   res.render("profileViews/characters", { characters });
 });
@@ -13,11 +13,7 @@ router.get("/characters/new", (req, res) => {
   res.render("profileViews/newCharacter");
 });
 
-<<<<<<< HEAD
-router.get("/characters/:Id/details", async (req, res) => {
-=======
 router.get("/characters/:Id/", async (req, res) => {
->>>>>>> master
   const character = await Character.findById(req.params.Id);
   res.render("profileViews/characterDetails", { character });
 });
@@ -42,9 +38,5 @@ router.post("/characters/new", async (req, res) => {
     res.redirect(`/characters/${created.id}`);
   }
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 
 module.exports = router;
