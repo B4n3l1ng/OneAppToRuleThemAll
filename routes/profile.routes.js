d5833ad260adb056e729bae54ae6479e70371894
B4n3l1ng <114490314+B4n3l1ng@users.noreply.github.com>
2022-11-04 14:01:46 +0000

    added the dealer routes

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Shop = require("../models/Shop.model");
+const User = require("../models/User.model");
 
 router.get("/shop", async (req, res) => {
   try {
@@ -24,4 +25,34 @@ router.get("/wealth", (req, res) => {
   res.render("profileViews/wealth");
 });
 
+router.get("/:userName/dealer", async (req, res) => {
+  const currentUser = await User.findOne({ username: req.params.userName });
+  res.render("profileViews/dealer", currentUser);
+});
+
+router.get("/:userName/dealer/dice", async (req, res) => {
+  try {
+    const playerDiceResult = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+    const dealerDiceResult = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+    const currentUser = await User.findOne({ username: req.params.userName });
+    const money = currentUser.money;
+    let result;
+    if (playerDiceResult > dealerDiceResult) {
+      result = "player";
+      await User.findOneAndUpdate(
+        { username: req.params.userName },
+        { money: money + 100 }
+      );
+      res.render("profileViews/dealer", { result });
+    } else if (dealerDiceResult > playerDiceResult) {
+      result = "dealer";
+      res.render("profileViews/dealer", { result });
+    } else if (playerDiceResult === dealerDiceResult) {
+      result = "draw";
+      res.render("profileViews/dealer", { result });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 module.exports = router;
