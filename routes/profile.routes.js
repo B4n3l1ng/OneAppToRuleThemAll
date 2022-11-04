cc196d4a07959870ce1db34132540f1ed777df6d
B4n3l1ng <114490314+B4n3l1ng@users.noreply.github.com>
2022-11-04 10:41:03 +0000

    file fix

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,38 +2,26 @@ const express = require("express");
 const router = express.Router();
 const Shop = require("../models/Shop.model");
 
-<<<<<<< HEAD
-router.get()
-
-
-
-=======
 router.get("/shop", async (req, res) => {
-    try {
-        const shopItems = await Shop.find()
-        console.log(shopItems)
-        res.render("profileViews/shop", { shopItems })
-     } catch (error) {
-        console.log(error)
-     }
-})
-
+  try {
+    const shopItems = await Shop.find();
+    console.log(shopItems);
+    res.render("profileViews/shop", { shopItems });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 router.get("/inventory", (req, res) => {
-    res.render("profileViews/inventory")
-})
+  res.render("profileViews/inventory");
+});
 
 router.get("/explore", (req, res) => {
-    res.render("profileViews/explore")
-})
+  res.render("profileViews/explore");
+});
 
 router.get("/wealth", (req, res) => {
-    res.render("profileViews/wealth")
-})
->>>>>>> master
-
-
-
-
+  res.render("profileViews/wealth");
+});
 
 module.exports = router;
