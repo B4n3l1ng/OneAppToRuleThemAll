451dd9c8b2a3e6cd2322aeac6495c4f7128359f4
B4n3l1ng <114490314+B4n3l1ng@users.noreply.github.com>
2022-11-08 14:57:41 +0000

    changeable nav bars

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  if (req.session.user) {
+    res.redirect("/profile");
+  } else {
+    res.render("index");
+  }
 });
 
 module.exports = router;
